c07c3a0ea29b1d618534b88d9de9681e50d7c4e2
Desmond Ndambi <dezondambi@gmail.com>
2021-11-27 04:26:32 +0100

    Initial Commit

---
+++ terraform-infra/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = "us-east-1"
+  profile                 = "ansible-terraform"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
+locals {
+  instance_type = "t2.micro"
+  key_name      = "ec2-terra-ansi"
+}
+
+resource "tls_private_key" "ec2_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.ec2_private_key.private_key_pem}' > ../${local.key_name}.pem"
+  }
+}
+
+resource "null_resource" "key-perm" {
+  depends_on = [tls_private_key.ec2_private_key]
+
+  provisioner "local-exec" {
+    command = "chmod 400 ../${local.key_name}.pem"
+  }
+}
+
+resource "aws_key_pair" "ec2_keys" {
+  key_name   = local.key_name
+  public_key = tls_private_key.ec2_private_key.public_key_openssh
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = local.instance_type
+  key_name      = aws_key_pair.ec2_keys.key_name
+
+  tags = {
+    Name = "jenkins-server"
+  }
+
+  # Install Jenkins according to https://www.digitalocean.com/community/tutorials/how-to-install-jenkins-on-ubuntu-20-04
+  # Install ansible and ensure community.general.terraform https://docs.ansible.com/ansible/latest/collections/community/general/terraform_module.html
+  # Install the plugins under the jenkins user, otherwise the community.general.terraform won't be found
+  user_data = <<EOF
+#!/bin/bash
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install default-jre -y
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo apt install ansible -y
+sudo su jenkins
+ansible-galaxy collection install community.general
+EOF
+}
+
+resource "aws_instance" "targets" {
+  count         = 2
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = local.instance_type
+  key_name      = aws_key_pair.ec2_keys.key_name
+
+  tags = {
+    Name = "target-server ${count.index}"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "nginx_container_name" {
+  type        = string
+  default     = "nginx-demo"
+  description = "Container name for nginx"
+}
+
